8e5e916d3ebe2747756b49127619051327a0e180
Arya Daniswara Khairan <arya.daniswara@ui.ac.id>
2023-10-31 14:07:01 +0700

    ADD dataset

--- TE1/generate_dataset.py
+++ TE1/generate_dataset.py
@@ -1,6 +1,6 @@
 import random
 
-def generate_dataset(n = 1):
+def generate_dataset_arraymode(n = 1):
     # Make Generate-Dataset Algorithm Here
     dataset = [0]*n
     for i in range(0,n-1):
--- TE1/main.py
+++ TE1/main.py
@@ -1,10 +1,20 @@
 from bcis import bidirectional_conditional_insertion_sort as do_bcsi
-from generate_dataset import generate_dataset
+from generate_dataset import generate_dataset_arraymode
 from counting_sort import counting_sort as do_cs
 
 '''ONLY FOR TESTING WHETHER THE CODES WORK'''
 
-A = generate_dataset(10)
+data_source = input("press 1 for dataset.txt | press 2 for generate new")
+if data_source == "1":
+    datas = open("dataset.txt", "r")
+    print(len(datas))
+    print(datas.readline())
+while True:
+    try:
+        A = generate_dataset_arraymode(int(input("Insert Dataset Length: ")))
+        break
+    except:
+        print("Wrong input type! Try Again")
 print("Original Order:", A)
 B = A.copy()
 C = A.copy()
